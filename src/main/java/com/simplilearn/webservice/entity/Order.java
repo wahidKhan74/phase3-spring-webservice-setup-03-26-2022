b8e159cb762fd6ad44c78005ad3f9ef76a5eb304
Wahid Khan <wahid.khan74@gmail.com>
2022-03-27 16:35:39 +0000

    added postman collection

---
+++ src/main/java/com/simplilearn/webservice/entity/Order.java
@@ -0,0 +1,6 @@
+package com.simplilearn.webservice.entity;
+
+public class Order {
+
+	// id, name, customer, email, totalPrice
+}
---
+++ src/main/java/com/simplilearn/webservice/entity/User.java
@@ -0,0 +1,6 @@
+package com.simplilearn.webservice.entity;
+
+public class User {
+	
+	// id, name, email, password, description, dateOfBith
+}
